73f4ce7bc8675c8014af597c1fb70957f4c44be0
hectorluiszambranopaz <hectorluiszambranopaz@gmail.com>
2024-10-26 21:38:43 -0500

    documento js

---
+++ JS/doc.js
@@ -0,0 +1,365 @@
+// productos (arrays,objetos,atributos)
+const productos = [
+    // Camiseta de tirantes
+    {
+      id: 1,
+      nombre: "Camiseta de tirantes",
+      cantidad_disponible: 30,
+      precio: 50000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 2,
+      nombre: "Camiseta de tirantes",
+      cantidad_disponible: 30,
+      precio: 120000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 3,
+      nombre: "Camiseta de tirantes",
+      cantidad_disponible: 30,
+      precio: 50000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 4,
+      nombre: "Camiseta de tirantes",
+      cantidad_disponible: 30,
+      precio: 50000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 5,
+      nombre: "Camiseta de tirantes",
+      cantidad_disponible: 30,
+      precio: 50000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+    // Pantalonetas
+    {
+      id: 6,
+      nombre: "Pantaloneta",
+      cantidad_disponible: 30,
+      precio: 65000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 7,
+      nombre: "Pantaloneta",
+      cantidad_disponible: 30,
+      precio: 65000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 8,
+      nombre: "Pantaloneta",
+      cantidad_disponible: 30,
+      precio: 65000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 9,
+      nombre: "Pantaloneta",
+      cantidad_disponible: 30,
+      precio: 65000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 10,
+      nombre: "Pantaloneta",
+      cantidad_disponible: 30,
+      precio: 65000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    // Camisetas
+    {
+      id: 11,
+      nombre: "camisetas",
+      cantidad_disponible: 30,
+      precio: 60000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 12,
+      nombre: "camisetas",
+      cantidad_disponible: 30,
+      precio: 60000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 13,
+      nombre: "camisetas",
+      cantidad_disponible: 30,
+      precio: 60000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 14,
+      nombre: "camisetas",
+      cantidad_disponible: 30,
+      precio: 60000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 15,
+      nombre: "camisetas",
+      cantidad_disponible: 30,
+      precio: 60000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+    // Zapatillas
+    {
+      id: 16,
+      nombre: "zapatillas",
+      cantidad_disponible: 30,
+      precio: 250000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 17,
+      nombre: "zapatillas",
+      cantidad_disponible: 30,
+      precio: 250000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 18,
+      nombre: "zapatillas",
+      cantidad_disponible: 30,
+      precio: 250000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 19,
+      nombre: "zapatillas",
+      cantidad_disponible: 30,
+      precio: 250000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 20,
+      nombre: "zapatillas",
+      cantidad_disponible: 30,
+      precio: 250000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+    // Chaquetas
+    {
+      id: 21,
+      nombre: "chaquetas",
+      cantidad_disponible: 30,
+      precio: 160000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 22,
+      nombre: "chaquetas",
+      cantidad_disponible: 30,
+      precio: 160000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 23,
+      nombre: "chaquetas",
+      cantidad_disponible: 30,
+      precio: 160000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 24,
+      nombre: "chaquetas",
+      ccantidad_disponible: 30,
+      precio: 160000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 25,
+      nombre: "chaquetas",
+      cantidad_disponible: 30,
+      precio: 160000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+    //Gorra deportiva
+    {
+      id: 26,
+      nombre: "Gorra Deportiva",
+      cantidad_disponible: 30,
+      precio: 40000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 27,
+      nombre: "Gorra Deportiva",
+      cantidad_disponible: 30,
+      precio: 40000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 28,
+      nombre: "Gorra Deportiva",
+      cantidad_disponible: 30,
+      precio: 40000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 29,
+      nombre: "Gorra Deportiva",
+      cantidad_disponible: 30,
+      precio: 40000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 30,
+      nombre: "Gorra Deportiva",
+      cantidad_disponible: 30,
+      precio: 40000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+    // Sudaderas
+    {
+      id: 31,
+      nombre: "susadaderas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 32,
+      nombre: "susadaderas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 33,
+      nombre: "susadaderas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 34,
+      nombre: "susadaderas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 35,
+      nombre: "susadaderas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    // Mallas
+    {
+      id: 36,
+      nombre: "mallas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 37,
+      nombre: "mallas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 38,
+      nombre: "mallas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 39,
+      nombre: "mallas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  
+    {
+      id: 40,
+      nombre: "mallas",
+      cantidad_disponible: 30,
+      precio: 80000,
+      categoria: "Entrenamiento",
+      imagen: "",
+    },
+  ];
